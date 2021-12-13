989a1fb695922547843b6ab208f687cd8332d700
Bharati Subramanian <bharatisharada@gmail.com>
2021-12-13 23:04:03 +0530

    style: ui for 0 available quickNotes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState("");
+  const [theme, setTheme] = useState(false);
+
   return (
-    <div className="App">
-     
-    </div>
+    <main className={`main ${theme ? "dark" : "light"}`}>
+      {
+        notes.length > 0 ? 
+        <div></div> 
+        :
+        <section className='empty-notes'>
+          <h1 className='empty-notes--head'>
+            You have 0 <span class="color-accent">quickNotes</span>
+          </h1>
+          <div className='empty-notes--new-note'>
+            <button className='btn btn-primary'>
+              <span className="new-note--icon">+</span>
+            </button>
+            <span className="new-note--text">Create your first quickNote</span>
+          </div>
+        </section>
+      }
+    </main>
   );
 }
 
